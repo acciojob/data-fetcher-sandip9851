cef5f957720536850e201a193864ad3d2c63e44d
sandip <mr.sandippradhan@gmail.com>
2024-07-25 10:09:32 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState,useEffect } from "react";
+import axios from "axios";
+import 'regenerator-runtime/runtime';
+
 
 const App = () => {
+  const[output,setOutput] = useState()
+  const [fetching,setFetching] = useState(true);
+
+  useEffect(()=>{
+    const api = "https://dummyjson.com/products"
+    const fetchData = async()=>{
+      try{
+        const response = await axios(api);
+        setOutput(response.data)
+      }catch(error){
+        console.error("error fetching the products",error)
+      }finally{
+        setFetching(false)
+      }
+    }
+    fetchData()
+
+  },[])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <u>Output : </u>
+       {fetching?<p>Loading...</p>:<div><h1>Data Fetched from Api</h1><pre>{JSON.stringify(output, null, 2)}</pre></div>}
     </div>
   )
 }
