f60b88bdbdca9ad70fcc3d3db4af3cd36770bf08
sandip <mr.sandippradhan@gmail.com>
2024-07-25 10:16:35 +0530

    error handeled

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,42 @@
-
-import React from "react";
-import './../styles/App.css';
-import { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import 'regenerator-runtime/runtime';
 
-
 const App = () => {
-  const[output,setOutput] = useState()
-  const [fetching,setFetching] = useState(true);
+  const [output, setOutput] = useState();
+  const [fetching, setFetching] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(()=>{
-    const api = "https://dummyjson.com/products"
-    const fetchData = async()=>{
-      try{
+  useEffect(() => {
+    const api = "https://dummyjson.com/products";
+    const fetchData = async () => {
+      try {
         const response = await axios(api);
-        setOutput(response.data)
-      }catch(error){
-        console.error("error fetching the products",error)
-      }finally{
-        setFetching(false)
+        setOutput(response.data);
+      } catch (error) {
+        setError('No data found');
+      } finally {
+        setFetching(false);
       }
-    }
-    fetchData()
-
-  },[])
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
       <u>Output : </u>
-       {fetching?<p>Loading...</p>:<div><h1>Data Fetched from Api</h1><pre>{JSON.stringify(output, null, 2)}</pre></div>}
+      {fetching ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          <h1>Data Fetched from API</h1>
+          <pre>{JSON.stringify(output, null, 2)}</pre>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
